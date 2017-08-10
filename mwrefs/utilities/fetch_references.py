3ee9a46a33bc749b31be3f4552b12acc83b579ad
Adam Roses Wight <awight@wikimedia.org>
2017-08-10 00:30:06 -0400

    Implement fetch_references
    This utility had been stubbed out with hard-coded parameters.

--- mwrefs/utilities/fetch_references.py
+++ mwrefs/utilities/fetch_references.py
@@ -3,13 +3,14 @@ Fetches the reference docs for a specified revision
 
 Usage:
     fetch_references -h | --help
-    fetch_references <host> <rev-id>
+    fetch_references <host> <title>
                      [--debug]
 
 Options:
     -h --help   Prints this documentation
-    <host>      The host of a MediaWiki instance to query
-    <rev-id>    The revision identifier to get text for
+    <host>      The host of a MediaWiki instance to query, e.g.
+                en.wikipedia.org
+    <title>     The article title to get text for
     --debug     Prints debug logging
 """
 import json
@@ -28,12 +29,12 @@ def main(argv=None):
         level=logging.WARNING if not args['--debug'] else logging.DEBUG,
         format='%(asctime)s %(levelname)s:%(name)s -- %(message)s'
     )
-    run(args['<host>'], int(args['<rev-id>']))
+    run(args['<host>'], args['<title>'])
 
 
-def run(host, rev_id):
-    session = mwapi.Session("https://en.wikipedia.org")
-    doc = session.get(action='query', prop='revisions', titles='Anachronism',
+def run(host, title):
+    session = mwapi.Session("https://" + host)
+    doc = session.get(action='query', prop='revisions', titles=title,
                       rvprop='content', formatversion=2)
     content = doc['query']['pages'][0]['revisions'][0]['content']
     for reference_doc in extract(content):
